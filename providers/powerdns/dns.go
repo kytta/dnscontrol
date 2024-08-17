89bd251eb3ffb3738c385c93e8cf249075ea8f5b
Nikita Karamov <me@kytta.dev>
2024-08-17 16:40:38 +0200

    Disable DNSSEC

--- providers/powerdns/dns.go
+++ providers/powerdns/dns.go
@@ -50,19 +50,7 @@ func (dsp *powerdnsProvider) GetZoneRecords(domain string, meta map[string]strin
 
 // GetZoneRecordsCorrections returns a list of corrections that will turn existing records into dc.Records.
 func (dsp *powerdnsProvider) GetZoneRecordsCorrections(dc *models.DomainConfig, existing models.Records) ([]*models.Correction, int, error) {
-	corrections, actualChangeCount, err := dsp.getDiff2DomainCorrections(dc, existing)
-	if err != nil {
-		return nil, 0, err
-	}
-
-	// DNSSec corrections
-	dnssecCorrections, err := dsp.getDNSSECCorrections(dc)
-	if err != nil {
-		return nil, 0, err
-	}
-	actualChangeCount += len(dnssecCorrections)
-
-	return append(corrections, dnssecCorrections...), actualChangeCount, nil
+	return dsp.getDiff2DomainCorrections(dc, existing)
 }
 
 // EnsureZoneExists creates a zone if it does not exist
